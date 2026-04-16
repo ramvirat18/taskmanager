42a8b7304787d47a26c3a01f6bcd4432cf176579
virat <ramvirat2020@gmail.com>
2026-04-17 00:28:21 +0530

    feat:added the login api

--- src/main/java/com/example/taskmanager/controller/UserController.java
+++ src/main/java/com/example/taskmanager/controller/UserController.java
@@ -2,6 +2,7 @@ package com.example.taskmanager.controller;
 
 import com.example.taskmanager.entity.User;
 import com.example.taskmanager.service.UserService;
+import org.springframework.security.core.parameters.P;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -18,5 +19,11 @@ public class UserController {
     public User registerUser( @RequestBody User user)
     {
         return userService.registerUser(user);
+    }
+
+    @PostMapping("/login")
+    public String login(@RequestParam String email, @RequestParam String password)
+    {
+        return userService.login(email,password);
     }
 }
--- src/main/java/com/example/taskmanager/service/UserService.java
+++ src/main/java/com/example/taskmanager/service/UserService.java
@@ -2,19 +2,37 @@ package com.example.taskmanager.service;
 
 import com.example.taskmanager.entity.User;
 import com.example.taskmanager.repository.UserRepository;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
 
     private final UserRepository userRepository;
+    private final BCryptPasswordEncoder passwordEncoder;
 
-    public UserService(UserRepository userRepository) {
+    public UserService(UserRepository userRepository, BCryptPasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     public User registerUser(User user)
     {
         return userRepository.save(user);
+    }
+
+    public String login(String email, String password) {
+
+        User user = userRepository.findByEmail(email).orElseThrow(()-> new RuntimeException("User not found"));
+
+        if(passwordEncoder.matches(password,user.getPassword()))
+        {
+            return "Login Successfully";
+        }
+        else
+        {
+            throw new RuntimeException("Invalid Credentials");
+        }
+
     }
 }
